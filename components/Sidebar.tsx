58a163632cfca17ee280aee809772a4e60591089
Stan Rutten <stanrutten242@hotmail.com>
2021-11-03 17:09:31 +0100

    update
    Added algolia response
    Added a Rangeinput component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,16 @@
 import { NextPage } from "next";
-import { RefinementList } from "react-instantsearch-dom";
+import {
+  RangeInput,
+  RatingMenu,
+  RefinementList,
+} from "react-instantsearch-dom";
 
 export default function Sidebar() {
   return (
     <div className="left-column">
       <h5>Brand</h5>
-      <RefinementList searchable={true} attribute="category" />
+      <RefinementList attribute="brand" />
+      <RangeInput attribute="price" />
     </div>
   );
 }
