446f706367d824a42e318b4c3fbf9c24e04d3067
Stan Rutten <stanrutten242@hotmail.com>
2021-11-05 11:32:46 +0100

    Add instantsearch error handling

--- components/SearchGrid.tsx
+++ components/SearchGrid.tsx
@@ -1,20 +1,42 @@
-import type { NextPage } from "next";
-import React from "react";
-import { InstantSearch, SearchBox } from "react-instantsearch-dom";
+import React, { Component } from "react";
+import {
+  InstantSearch,
+  SearchBox,
+  connectStateResults,
+} from "react-instantsearch-dom";
 import Sidebar from "../components/Sidebar";
 import Content from "../components/Content";
 import algoliasearch from "algoliasearch/lite";
-import HitComponent from "../components/HitComponent";
 
 const agolia = algoliasearch(
   process.env.NEXT_PUBLIC_API_NAME!,
   process.env.NEXT_PUBLIC_API_KEY!
 );
 
+const SearchErrorCatcher = connectStateResults(
+  class SearchErrorCatcher extends Component {
+    error = null;
+
+    componentWillReceiveProps(nextProps: any) {
+      if (this.error && !nextProps.error) {
+        this.error = null;
+      }
+      if (!this.error && nextProps.error) {
+        // only throw on new errors
+        throw nextProps.error;
+      }
+    }
+    render() {
+      return null;
+    }
+  }
+);
+
 function SearchGrid({ searchClient }: any) {
   return (
     <>
       <InstantSearch searchClient={searchClient} indexName={"instant_search"}>
+        <SearchErrorCatcher />
         <header className="header">
           <SearchBox translations={{ placeholder: "Search for products" }} />
         </header>
