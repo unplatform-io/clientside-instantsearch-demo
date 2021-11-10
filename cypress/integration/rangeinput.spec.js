379df493a1b42fd45b86b82b0878c15944d85254
Stan Rutten <stanrutten242@hotmail.com>
2021-11-10 09:23:51 +0100

    Added semicolon

--- cypress/integration/rangeinput.spec.js
+++ cypress/integration/rangeinput.spec.js
@@ -4,45 +4,45 @@ describe("Range input search", () => {
     });
 
     it("Empty page", () => {
-        cy.get(".ais-Stats-text").contains(20)
-        cy.get(".hit-name").eq(0).contains("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
+        cy.get(".hit-name").eq(0).contains("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops");
+        cy.get(".ais-Stats-text").contains(20);
     });
 
     it("Range input price", () => {
-        cy.get(".ais-RangeInput-input--min").eq(0).type(10)
-        cy.get(".ais-RangeInput-input--max").eq(0).type(50)
-        cy.get(".ais-RangeInput-submit").eq(0).click()
+        cy.get(".ais-RangeInput-input--min").eq(0).type(10);
+        cy.get(".ais-RangeInput-input--max").eq(0).type(50);
+        cy.get(".ais-RangeInput-submit").eq(0).click();
 
-        cy.get(".hit-name").eq(0).contains("Mens Casual Premium Slim Fit T-Shirts")
-        cy.get(".ais-Stats-text").contains(6)
+        cy.get(".hit-name").eq(0).contains("Mens Casual Premium Slim Fit T-Shirts");
+        cy.get(".ais-Stats-text").contains(6);
     });
 
     it("Range input price && id", () => {
-        cy.get(".ais-RangeInput-input--min").eq(0).type(10)
-        cy.get(".ais-RangeInput-input--max").eq(0).type(50)
-        cy.get(".ais-RangeInput-submit").eq(0).click()
+        cy.get(".ais-RangeInput-input--min").eq(0).type(10);
+        cy.get(".ais-RangeInput-input--max").eq(0).type(50);
+        cy.get(".ais-RangeInput-submit").eq(0).click();
 
 
-        cy.get(".ais-RangeInput-input--min").eq(1).type(5)
-        cy.get(".ais-RangeInput-input--max").eq(1).type(10)
-        cy.get(".ais-RangeInput-submit").eq(1).click()
+        cy.get(".ais-RangeInput-input--min").eq(1).type(5);
+        cy.get(".ais-RangeInput-input--max").eq(1).type(10);
+        cy.get(".ais-RangeInput-submit").eq(1).click();
 
-        cy.get(".hit-name").eq(0).contains("Pierced Owl Rose Gold Plated Stainless Steel Double")
-        cy.get(".ais-Stats-text").contains(1)
+        cy.get(".hit-name").eq(0).contains("Pierced Owl Rose Gold Plated Stainless Steel Double");
+        cy.get(".ais-Stats-text").contains(1);
     });
 
     it("Range input search && price && id ", () => {
-        cy.get(".ais-RangeInput-input--min").eq(0).type(25)
-        cy.get(".ais-RangeInput-input--max").eq(0).type(500)
-        cy.get(".ais-RangeInput-submit").eq(0).click()
+        cy.get(".ais-RangeInput-input--min").eq(0).type(25);
+        cy.get(".ais-RangeInput-input--max").eq(0).type(500);
+        cy.get(".ais-RangeInput-submit").eq(0).click();
 
 
-        cy.get(".ais-RangeInput-input--min").eq(1).type(5)
-        cy.get(".ais-RangeInput-input--max").eq(1).type(10)
-        cy.get(".ais-RangeInput-submit").eq(1).click()
+        cy.get(".ais-RangeInput-input--min").eq(1).type(5);
+        cy.get(".ais-RangeInput-input--max").eq(1).type(10);
+        cy.get(".ais-RangeInput-submit").eq(1).click();
 
-        cy.get(".ais-SearchBox-input").type('gold')
-        cy.get(".hit-name").eq(0).contains("Solid Gold Petite Micropave")
-        cy.get(".ais-Stats-text").contains(1)
+        cy.get(".ais-SearchBox-input").type('gold');
+        cy.get(".hit-name").eq(0).contains("Solid Gold Petite Micropave");
+        cy.get(".ais-Stats-text").contains(1);
     });
 });
